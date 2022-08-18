dac8c0792224e7ac757ad24fba17779c13dee788
Piyawat Saelim <6310110285@psu.ac.th>
2022-08-19 01:11:32 +0700

    part 6

--- src/WordCard.js
+++ src/WordCard.js
@@ -1,15 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import _ from 'lodash'
 import CharacterCard from "./CharacterCard";
+
+const prepareStateFromWord = (given_word) => {
+    let word = given_word.toUpperCase()
+    let chars = _.shuffle(Array.from(word))
+    return{
+        word,
+        chars,
+        attempt: 1,
+        guess: '',
+        completed: false
+    }
+}
+
 export default function WordCard(props){
+
+    const [state, setState] = useState(prepareStateFromWord(props.value))
 
     const activationHandler = c => {
         console.log(`${c} has been activated.`)
+
+        let guess = state.guess + c
+        setState({...state, guess: guess})
+        
+        console.log(guess)
+
+        if(guess.length == state.word.length){
+            if(guess == state.word){
+                console.log('yeah!! You can SPELL!')
+                setState({...state, completed: true})
+            }
+            else{
+                console.log('reset, try again')
+                setState({...state, guess: '', attempt: state.attempt + 1})
+            }
+        }
     }
     return (
         <div>
             {
-                Array.from(props.value).map((c, i) => 
-                    <CharacterCard value={c} key={i} activationHandler/>
+                state.chars.map((c, i) => 
+                    <CharacterCard value={c} key={i} activationHandler={activationHandler} attempt={state.attempt}/>
                 )
             }
         </div>
